3f48546dde93e1efe15111f8881f6668fdc390e8
Kutluhan Duman <kutluduman@gmail.com>
2020-08-03 19:30:54 +0000

    Installed body-parser package and added post route

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -33,6 +36,11 @@ app.get('/urls.json', (req,res) => {
 
 app.get('/hello', (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post('/urls', (req,res) => {
+  console.log(req.body);
+  res.send("OK");
 });
  
 
