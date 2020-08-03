a923e02f5ee7c752b9ab2006c5614c6e245bf685
Kutluhan Duman <kutluduman@gmail.com>
2020-08-03 18:32:10 +0000

    Added route with parameter where respond could use the parameter for shortening the url

--- express_server.js
+++ express_server.js
@@ -13,6 +13,11 @@ app.get('/', (req,res) => {
   res.send('Hello!');
 });
 
+app.get('/urls/:shortURL', (req,res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase};
+  res.render("urls_show", templateVars);
+});
+
 app.get('/urls', (req,res) => {
   let templateVars = { urls : urlDatabase};
   res.render('urls_index.ejs',templateVars);
