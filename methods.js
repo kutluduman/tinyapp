260b51b22c07d983e8799f51d8564c1b81e835c4
Kutluhan Duman <kutluduman@gmail.com>
2020-08-05 22:04:56 +0000

    Exported functions and objects for express_server

---
+++ methods.js
@@ -0,0 +1,39 @@
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+
+const randomString = () => {
+  let random = Math.random().toString(36).substring(2,8);
+  return random;
+};
+
+
+/*
+This function checks if there is an email that is same as users object email
+*/
+const isEmailRegistered = (email) => {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return false;
+};
+
+module.exports = {urlDatabase,users,randomString,isEmailRegistered};
