fe48d643f37b1b7865e04fa62c159bb3d2e07b38
Kutluhan Duman <kutluduman@gmail.com>
2020-08-05 22:05:30 +0000

    Configured require in order to receive functions and objects from methods file

--- express_server.js
+++ express_server.js
@@ -1,46 +1,9 @@
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-
-const users = {
-  "userRandomID": {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur"
-  },
-  "user2RandomID": {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk"
-  }
-};
-
-
-const randomString = () => {
-  let random = Math.random().toString(36).substring(2,8);
-  return random;
-};
-
-
-/*
-This function checks if there is an email that is same as users object email
-*/
-const isEmailRegistered = (email) => {
-  for (const user in users) {
-    if (users[user].email === email) {
-      return users[user];
-    }
-  }
-  return false;
-};
-
 const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const {urlDatabase,users,randomString,isEmailRegistered} = require('./methods');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
