6d1c1800924d7978929a89dd0c61bbd55c4266e4
Kutluhan Duman <kutluduman@gmail.com>
2020-08-05 12:12:33 +0000

    Users object is added

--- express_server.js
+++ express_server.js
@@ -15,6 +15,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(cookieParser());
 
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
+
 
 
 const urlDatabase = {
