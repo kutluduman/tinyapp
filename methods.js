4d7450160620dab074c247693e02f95c0f7a606e
Kutluhan Duman <kutluduman@gmail.com>
2020-08-06 18:12:23 +0000

    Added bcrypt to the file so that the existing passwords can be hashed as well

--- methods.js
+++ methods.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const urlDatabase = {
   b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
   i3BoGr: { longURL: "https://www.google.ca", userID: "aJ48lW" }
@@ -7,12 +9,12 @@ const users = {
   "userRandomID": {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur"
+    password:  bcrypt.hashSync("purple-monkey-dinosaur", 10)
   },
   "user2RandomID": {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk"
+    password: bcrypt.hashSync("dishwasher-funk", 10)
   }
 };
 
